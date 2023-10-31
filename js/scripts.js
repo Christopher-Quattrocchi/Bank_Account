f4ba015c2174cdfb0bf69460218269279626d8ae
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-10-31 16:30:32 -0700

    WIP adding UI.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-function BankAccount (name, initialDeposit) {
-    this.name = name;
-    this.balance = initialDeposit;
+//Business Logic
+function BankAccount(name, initialDeposit) {
+  this.name = name;
+  this.balance = initialDeposit;
 }
 
-BankAccount.prototype.depositMoney = function(deposit) {
+BankAccount.prototype.depositMoney = function (deposit) {
   if (deposit > 0) {
     this.balance += deposit;
     console.log("After depositing you have: " + this.balance)
@@ -12,11 +13,49 @@ BankAccount.prototype.depositMoney = function(deposit) {
   }
 };
 
-BankAccount.prototype.withdrawMoney = function(withdraw) {
-    if (withdraw > 0 && (this.balance >= withdraw)) {
-        this.balance -= withdraw;
-        console.log("After withdrawal you have: " + this.balance)
-    } else {
-      console.log("You don't have enough money")
+BankAccount.prototype.withdrawMoney = function (withdraw) {
+  if (withdraw > 0 && (this.balance >= withdraw)) {
+    this.balance -= withdraw;
+    console.log("After withdrawal you have: " + this.balance)
+  } else {
+    console.log("You don't have enough money")
+  }
+}
+
+//UI Logic
+let myAccount;
+window.addEventListener("load", function () {
+  document.getElementById("register").addEventListener("submit", handleCreation)
+  document.getElementById("change-balance").addEventListener("submit", handleChangeBalance);
+})
+
+function handleCreation(e) {
+  e.preventDefault();
+  const name = document.getElementById("input-name").value;
+  const initialDeposit = parseInt(document.getElementById("initial-deposit").value);
+
+  myAccount = new BankAccount(name, initialDeposit);
+  console.log(myAccount);
+
+
+}
+
+function handleChangeBalance(e) {
+  e.preventDefault();
+  if (myAccount) {
+    const deposit = parseInt(document.getElementById("deposit").value);
+    const withdraw = parseInt(document.getElementById("withdraw").value);
+    if (!isNaN(deposit) && deposit > 0) {
+      myAccount.depositMoney(deposit);
+      console.log(myAccount.balance);
+    } else if (withdraw) {
+      myAccount.withdrawMoney(withdraw);
+      console.log(myAccount.balance);
     }
+    else {
+      console.log("Please enter a valid withdrawal or deposit");
+    }
+  } else {
+    console.log("create an account");
+  }
 }
