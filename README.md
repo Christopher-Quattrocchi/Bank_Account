c80059f672541782704358c7f89227b760d27cc3
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-10-31 15:58:06 -0700

    Add complete Test #3.
    Co-authored-by Christopher Quattrocchi <chrisquattrocchi@gmail.com>
    Co-authored-by: Brianca Knight <briancaknight@gmail.com>

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ let user1 = new BankAccount("Bill", 5);
 user1;
 __Expected Output:__ Account {name: 'Bill', initialDeposit: 5}
 
-__Test #2:__
+__Test #2:__ "It will allow the user to deposit money"
 __Code:__
 let user1 = new BankAccount("Bill", 5);
 user1.depositMoney(5);
 user1;
 __Expected Output:__ BankAccount {name: 'Bill', balance: 10}
+
+__Test #3:__ "It will allow the user to withdraw money"
+__Code:__
+let user1 = new BankAccount("Bill", 10);
+user1.withdrawMoney(5);
+user1;
+__Expected Output:__ BankAccount {name: 'Bill', balance: 5}
--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,19 @@ function BankAccount (name, initialDeposit) {
 }
 
 BankAccount.prototype.depositMoney = function(deposit) {
-  this.balance += deposit;
+  if (deposit > 0) {
+    this.balance += deposit;
+    console.log("After depositing you have: " + this.balance)
+  } else {
+    console.log("Enter a positive number");
+  }
+};
+
+BankAccount.prototype.withdrawMoney = function(withdraw) {
+    if (withdraw > 0 && (this.balance >= withdraw)) {
+        this.balance -= withdraw;
+        console.log("After withdrawal you have: " + this.balance)
+    } else {
+      console.log("You don't have enough money")
+    }
 }
