8dd330fbfca5e1fbc005a9ca1f2b50637be4a1be
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-10-31 21:22:59 -0700

    Add complete UI with additional features.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,72 @@
-//Business Logic
+// Business Logic
+
+function RegisteredAccount() {
+  this.accounts = [];
+  this.currentId = 0;
+}
+
+RegisteredAccount.prototype.addAccount = function(account) {
+  account.id = this.assignId();
+  this.accounts[account.id] = account;
+  this.accounts.push(account);
+  console.log(account)
+}
+
+RegisteredAccount.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+let registeredAccounts = new RegisteredAccount();
+
 function BankAccount(name, initialDeposit) {
   this.name = name;
   this.balance = initialDeposit;
 }
 
-BankAccount.prototype.depositMoney = function (deposit) {
+BankAccount.prototype.depositMoney = function(deposit) {
   if (deposit > 0) {
     this.balance += deposit;
-    console.log("After depositing you have: " + this.balance)
+    depositSuccess(deposit, this.balance);
   } else {
-    console.log("Enter a positive number");
+    showError("Enter a positive number");
+  }
+};
+BankAccount.prototype.withdrawMoney = function(withdraw) {
+  if (withdraw > 0 && this.balance >= withdraw) {
+    this.balance -= withdraw;
+    withdrawSuccess(withdraw, this.balance);
+  } else {
+    showError("You don't have enough money");
   }
 };
 
-BankAccount.prototype.withdrawMoney = function (withdraw) {
-  if (withdraw > 0 && (this.balance >= withdraw)) {
-    this.balance -= withdraw;
-    console.log("After withdrawal you have: " + this.balance)
-  } else {
-    console.log("You don't have enough money")
-  }
-}
-
-//UI Logic
+// UI Logic
 let myAccount;
-window.addEventListener("load", function () {
-  document.getElementById("register").addEventListener("submit", handleCreation)
+window.addEventListener("load", function() {
+  document.getElementById("register").addEventListener("submit", handleAccountCreation);
   document.getElementById("change-balance").addEventListener("submit", handleChangeBalance);
-})
+  document.querySelector("a#log-out").addEventListener("click", logOut);
+  document.querySelector("a#displayDeposit").addEventListener("click", depositDisplay);
+  document.querySelector("a#displayWithdraw").addEventListener("click", withdrawDisplay)
+});
 
-function handleCreation(e) {
+function handleAccountCreation(e) {
   e.preventDefault();
+  reset();
   const name = document.getElementById("input-name").value;
   const initialDeposit = parseInt(document.getElementById("initial-deposit").value);
+  if (isNaN(initialDeposit) || initialDeposit <= 0) {
+    showError("Please enter a valid initial deposit.");
+    return;
+  }
 
   myAccount = new BankAccount(name, initialDeposit);
-  console.log(myAccount);
+  registeredAccounts.addAccount(myAccount);
 
-
+  toggleVisibility("changeBalancePage");
+  toggleVisibility("register");
+  toggleVisibility("log-out");
 }
 
 function handleChangeBalance(e) {
@@ -45,17 +74,73 @@ function handleChangeBalance(e) {
   if (myAccount) {
     const deposit = parseInt(document.getElementById("deposit").value);
     const withdraw = parseInt(document.getElementById("withdraw").value);
+
     if (!isNaN(deposit) && deposit > 0) {
       myAccount.depositMoney(deposit);
-      console.log(myAccount.balance);
-    } else if (withdraw) {
+    } else if (!isNaN(withdraw) && withdraw > 0) {
       myAccount.withdrawMoney(withdraw);
-      console.log(myAccount.balance);
-    }
-    else {
-      console.log("Please enter a valid withdrawal or deposit");
+    } else {
+      showError("Please enter a valid withdrawal or deposit.");
     }
   } else {
-    console.log("create an account");
+    showError("Please create an account before trying to deposit.");
+  }
+}
+
+function logOut() {
+  toggleVisibility("register");
+  toggleVisibility("changeBalancePage");
+  toggleVisibility("log-out");
+}
+function depositSuccess(deposit, balance) {
+  document.querySelector("input#deposit").value = null;
+  updateResult("Your deposit of $" + deposit + " was successful!", balance);
+}
+function withdrawSuccess(withdraw, balance) {
+  document.querySelector("input#withdraw").value = null;
+  updateResult("Your withdrawal of $" + withdraw + " was successful!", balance);
+}
+function showError(message) {
+  alert(message);
+}
+function reset() {
+  updateResult(null, null);
+}
+function updateResult(message, balance) {
+  document.querySelector("#result").innerHTML = message;
+  document.querySelector("#current-balance").innerHTML = balance;
+}
+
+function toggleVisibility(elementId) {
+  document.querySelector(`#${elementId}`).classList.toggle("hidden");
+}
+
+function depositDisplay() {
+  const depositDisplay = document.querySelector("div#depositDisplay");
+  const withdrawDisplay = document.querySelector("div#withdrawDisplay");
+
+  if (depositDisplay.classList.contains("hidden") && withdrawDisplay.classList.contains("hidden")) {
+    depositDisplay.classList.remove("hidden");
+  } else if (depositDisplay.classList.contains("hidden") && !withdrawDisplay.classList.contains("hidden")) {
+    depositDisplay.classList.remove("hidden");
+    withdrawDisplay.classList.add("hidden");
+  } else {
+    depositDisplay.classList.add("hidden");
+    withdrawDisplay.classList.add("hidden");
+  }
+}
+
+function withdrawDisplay() {
+  const depositDisplay = document.querySelector("div#depositDisplay");
+  const withdrawDisplay = document.querySelector("div#withdrawDisplay");
+
+  if (depositDisplay.classList.contains("hidden") && withdrawDisplay.classList.contains("hidden")) {
+    withdrawDisplay.classList.remove("hidden");
+  }  else if (withdrawDisplay.classList.contains("hidden") && !depositDisplay.classList.contains("hidden")) {
+    withdrawDisplay.classList.remove("hidden");
+    depositDisplay.classList.add("hidden");
+  } else {
+    depositDisplay.classList.add("hidden");
+    withdrawDisplay.classList.add("hidden");
   }
 }
